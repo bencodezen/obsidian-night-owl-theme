7e6bcbb68b148049ef9a05b6545be26e5135d985
Ben Hong <ben@bencodezen.io>
2021-01-01 08:10:56 -0500

    config: centralize theme in one area

--- index.js
+++ index.js
@@ -6,10 +6,5 @@ dotenv.config()
 
 fse
   .copy('./obsidian-night-owl-theme.css', process.env.DESTINATION)
-  .then(() => console.log('Destination #1: CSS file copied successfully!'))
-  .catch(error => console.error(error))
-
-fse
-  .copy('./obsidian-night-owl-theme.css', process.env.DESTINATION_TWO)
-  .then(() => console.log('Destination #2: CSS file copied successfully!'))
+  .then(() => console.log('Destination: CSS file copied successfully!'))
   .catch(error => console.error(error))
