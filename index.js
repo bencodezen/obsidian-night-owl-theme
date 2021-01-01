67fc2e2c5a7f81079426aac4fd33a937d2a2caf1
Ben Hong <ben@bencodezen.io>
2021-01-01 07:34:31 -0500

    config: added config for two vaults

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ dotenv.config()
 
 fse
   .copy('./obsidian-night-owl-theme.css', process.env.DESTINATION)
-  .then(() => console.log('CSS file copied successfully!'))
+  .then(() => console.log('Destination #1: CSS file copied successfully!'))
+  .catch(error => console.error(error))
+
+fse
+  .copy('./obsidian-night-owl-theme.css', process.env.DESTINATION_TWO)
+  .then(() => console.log('Destination #2: CSS file copied successfully!'))
   .catch(error => console.error(error))
